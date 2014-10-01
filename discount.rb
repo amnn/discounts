6b6c06eb14401792c9c73c2454f6e27c9398d560
Ashok Menon <amenon94@gmail.com>
2014-10-01 12:46:20 +0100

    Adding method to generate matrix for deals and order.

--- discount.rb
+++ discount.rb
@@ -1,12 +1,13 @@
 require './sparse_matrix'
 
-def calculate_discount(deals, order)
-  matrix =
-    SparseMatrix.new(
-      deals.flat_map { |d| d.apply(order) },
-      order) do |discount, item|
-        discount.items.include? item.item_id
-      end
+def deal_matrix(deals, order)
+  SparseMatrix.new(
+    deals.flat_map { |d| d.apply(order) },
+    order) do |discount, item|
+      discount.items.include? item.item_id
+    end
+end
 
-  matrix
+def calculate_discount(deals, order)
+  matrix = deal_matrix(deals, order)
 end
